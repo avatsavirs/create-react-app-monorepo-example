38c72cd02e55a0e927bfc7037bbc8db7f3ca6950
avatsavirs <kartikey.srivastava@healthify.co>
2022-10-28 11:09:44 +0530

    fix: autoimport jsx

--- packages/app/App.js
+++ packages/app/App.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
 import SomeComponent from 'some-component';
 import * as hooks from '@pkg/hooks';
 import { Alert, Affix, Badge } from '@pkg/components'
 import './App.css';
 import { Container } from './App.styled'
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Container>
-            <h1 className="App-title">Welcome to React</h1>
-          </Container>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>packages/app/App.js</code> and save to reload.
-        </p>
-        <SomeComponent />
-        {
-          Object.keys(hooks).join('<>')
-        }
-        <div>
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <h1 className="App-title">Welcome to React</h1>
+        </Container>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>packages/app/App.js</code> and save to reload.
+      </p>
+      <SomeComponent />
+      {
+      Object.keys(hooks).join('<>')
+    }
+      <div>
         <Badge count={100} title="badge" />
         <Alert message="tessssssst" description="dessccc" />
         <Affix offsetTop={10}>
           <Alert message="fix" description="ne" />
         </Affix>
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
